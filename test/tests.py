48fc9abbe84ef4968b36694f090f6ee37fcb462b
Sourabh Mehta <sourabh.mehta@arm.com>
2023-07-21 13:35:13 +0200

    Fixed review feedback

--- test/tests.py
+++ test/tests.py
@@ -9,14 +9,27 @@ def test_check_archive(base_path):
     dirDict["bin"] = {'cbuild', 'cbuildgen', 'cpackget', 'csolution', 'packchk', 'svdconv'}
     dirDict["doc"] = {'index.html'}
     dirDict["etc"] = {
-        "AC6.6.18.0.cmake", "ac6_linker_script.sct", "gcc_linker_script.ld",
-        "iar_linker_script.icf", "CPRJ.xsd", "PACK.xsd", "GCC.10.3.1.cmake",
-        "CLANG.16.0.0.cmake", "IAR.9.32.1.cmake",
-        "{{ProjectName}}.cproject.yml", "{{SolutionName}}.csolution.yml",
-        "clayer.schema.json", "common.schema.json", "cproject.schema.json",
-        "cproject.schema.json", "CMakeASM",
-        "cbuild.schema.json", "cdefault.schema.json", "cdefault.yml",
-        "clang_linker_script.ld", "CMSIS-Build-Utils.cmake", "csolution.schema.json"}
+        "{{ProjectName}}.cproject.yml",
+        "{{SolutionName}}.csolution.yml",
+        "AC6.6.18.0.cmake",
+        "ac6_linker_script.sct",
+        "cbuild.schema.json",
+        "cdefault.schema.json",
+        "cdefault.yml",
+        "CLANG.16.0.0.cmake",
+        "clang_linker_script.ld",
+        "clayer.schema.json",
+        "CMakeASM",
+        "CMSIS-Build-Utils.cmake",
+        "common.schema.json",
+        "CPRJ.xsd",
+        "cproject.schema.json",
+        "csolution.schema.json"
+        "GCC.10.3.1.cmake",
+        "gcc_linker_script.ld",
+        "IAR.9.32.1.cmake",
+        "iar_linker_script.icf",
+        "PACK.xsd"}
 
     assert(True == path.isfile(path.join(base_path,"LICENSE.txt")))
     for dir in dirDict:
