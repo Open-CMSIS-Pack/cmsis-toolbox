b4ca11f7493f0ae58ccb15c7552cef9c49006564
Sourabh Mehta <sourabh.mehta@arm.com>
2024-06-05 09:26:03 +0200

    Added binary version info used to run tests

--- test/lib/utils.py
+++ test/lib/utils.py
@@ -1,5 +1,8 @@
 import glob
 from pathlib import Path
+import shutil
+import subprocess
+import re
 
 def glob_files_in_directory(directory:str, pattern: str, recursive: bool):
     yaml_files = glob.glob(directory + '/**/' + pattern, recursive=recursive)
@@ -11,3 +14,26 @@ def get_parent_directory_name(file_path:str):
 
 def get_parent_directory_path(file_path:str):
     return Path(file_path).parent.absolute()
+
+def write_test_environment(test_env_file:str):
+    toolList = ["cbuild", "cpackget", "csolution", "cbuild2cmake", "cbuildgen"]
+
+    # Create markdown content
+    markdown_content = "|Name|Version|\n"
+    markdown_content += "|:---|:------|\n"
+
+    for index in range(0, len(toolList)):
+        version = "unknown"
+        tool = toolList[index]
+        versionCmd = tool + ' -V'
+        output = subprocess.run(versionCmd, shell=True, universal_newlines=True, capture_output=True)
+        if output.stdout != "":
+            result = re.search(r"(\d+\.\d+\.\d+.*) \(C\)", output.stdout)
+            if result:
+                if len(result.groups()) > 0:
+                    version = result.group(1)
+        markdown_content += "|" + tool + "|" + version + "|\n"
+
+    # Write tool version info
+    with open(test_env_file, "w") as file:
+        file.write(markdown_content)
