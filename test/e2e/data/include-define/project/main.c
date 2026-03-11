7ef8c6e2bd53dced11df1f60c6ece0a93581e403
Jen-Tse Huang <jen-tse.huang@arm.com>
2026-03-11 11:24:01 +0100

    Update test for [cbuild2cmake] fix

--- test/e2e/data/include-define/project/main.c
+++ test/e2e/data/include-define/project/main.c
@@ -1,6 +1,16 @@
 #include "RTE_Components.h"
 #include CMSIS_device_header
 
+#include RANDOM_CONFIG_FILE_1
+#include RANDOM_CONFIG_FILE_2
+
+#ifndef RANDOM_CONFIG1_INCLUDED
+#error "Header <random_config1.h> include failed"
+#endif
+#ifndef RANDOM_CONFIG2_INCLUDED
+#error "Header \"random_config2.h\" include failed"
+#endif
+
 int main(void) {
   return 0;
 }
